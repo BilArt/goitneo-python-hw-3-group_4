658c9b5c2a8e980a879c492392cfccfc101ae9ec
Artem Bilousov <artem.bilousov.1993@gmail.com>
2023-12-16 18:11:44 +0100

    add def get_birthdays_per_week in class AddressBook

--- contacts_book.py
+++ contacts_book.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 class Field:
     def __init__(self, value):
         self.value = value
@@ -19,12 +21,23 @@ class Phone(Field):
     def validate_phone(self):
         if not self.value.isdigit() or len (self.value) != 10:
             raise ValueError("Phone must be a 10-digit numeric value.")
-        
+
+class Bithday(Field):
+    def __init__(self, value):
+        super().__init__(value)
+        self.validate_birhday()
+
+    def validate_birthday(self):
+        try:
+            datetime.strptime(self.value, "%d.%m.%Y")
+        except ValueError:
+            raise ValueError("Invalid birthday format. Use DD.MM.YYYY")
 
 class Record:
     def __init__(self, name):
         self.name = Name(name)
         self.phones = []
+        self.birthday = None
 
     def add_phone(self, phone):
         new_phone = Phone(phone)
@@ -37,6 +50,9 @@ class Record:
         self.remove_phone(old_phone)
         self.add_phone(new_phone)
 
+    def add_birthday(self, birthday):
+        self.birthday = Birthday(birthday)
+
     def find_phone(self, phone):
         for p in self.phones:
             if p.value == phone:
@@ -45,7 +61,8 @@ class Record:
                          
     def __str__(self):
         phones_str = ': '.join(str(p) for p in self.phones)
-        return f"Contact name: {self.name.value}, phones: {phones_str}"
+        birthday_str = f", Birthday: {self.birthday}" if self.birthday else ""
+        return f"Contact name: {self.name.value}, phones: {phones_str}{birthday_str}"
     
 
 class AddressBook:
@@ -63,6 +80,19 @@ class AddressBook:
             del self.data[name]
         else:
             raise ValueError("Record not found.")
+
+    def get_birthdays_per_week(self):
+        today = datetime.today()
+        next_week = today + timedelta(days=7)
+        upcoming_birthdays = []
+
+        for record in self.data.values():
+            if record.birthday:
+                birthday_date = datetime.strptime(record.birthday.value, "%d.%m.%Y").replace(year=today.year)
+                if today <= birthday_date < next_week:
+                    upcoming_birthdays.append(record.name.value)
+
+        return upcoming_birthdays
         
 
 # Створення нової адресної книги
